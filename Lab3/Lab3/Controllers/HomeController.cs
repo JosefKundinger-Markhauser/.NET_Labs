b1b68911a8b5787e1535986fa74fa21c6ad01980
Josef Kundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-06-09 13:25:42 -0400

    added lab2 and lab3

---
+++ Lab3/Lab3/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Count()
+        {
+            ViewBag.count = Request.Form["count"];
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return View();
+        }
+
+        public IActionResult TestError()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab3/Lab3/Models/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+
+        [Required]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
