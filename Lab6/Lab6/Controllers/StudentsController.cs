51b29c558e1423e9b75f13e1e5c20ead56613563
JosefKundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-07-20 19:59:38 -0400

    Lab6 Finished

---
+++ Lab6/Lab6/Controllers/StudentsController.cs
@@ -0,0 +1,166 @@
+﻿using Lab6.Data;
+using Lab6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        /// <summary>
+        /// Get collection of Students.
+        /// </summary>
+        /// <returns>A colection of Students</returns>
+        /// <response code="200">Returns a collection of Students</response>
+        /// <response code="500">Internal error</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<Student>>> GetAllStudents()
+        {
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        // GET: Students/d2cab0c9-7e94-409e-2b9a-08d88428ae4a
+        /// <summary>
+        /// Get a Student.
+        /// </summary>
+        /// <param id="id"></param>
+        /// <returns>A Student</returns>
+        /// <response code="201">Returns a collection of Students</response>
+        /// <response code="400">If the id is malformed</response>      
+        /// <response code="404">If the Student is null</response>      
+        /// <response code="500">Internal error</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Student>> GetById(Guid id)
+        {
+            Student student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // POST: Students
+        /// <summary>
+        /// Creates a Student.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Students
+        ///     {
+        ///        "FirstName": "FirstName",
+        ///        "LastName": "LastName",
+        ///        "Program": "Program"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>A newly created Student</returns>
+        /// <response code="201">Returns the newly created Student</response>
+        /// <response code="400">If the Student is malformed</response>      
+        /// <response code="500">Internal error</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Student>> CreateStudent([Bind("FirstName,LastName,Program")] StudentBase studentBase)
+        {
+            Student student = new Student
+            {
+                FirstName = studentBase.FirstName,
+                LastName = studentBase.LastName,
+                Program = studentBase.Program
+            };
+
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = student.ID }, student);
+        }
+
+        // DELETE: Students
+        /// <summary>
+        /// Deletes a Student.
+        /// </summary>
+        /// <param id="id"></param>
+        /// <response code="202">Student is deleted</response>
+        /// <response code="400">If the id is malformed</response>      
+        /// <response code="500">Internal error</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            Student student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return Accepted();
+        }
+
+        // PUT: Students
+        /// <summary>
+        /// Upserts a Student.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Students
+        ///     {
+        ///        "FirstName": "FirstName",
+        ///        "LastName": "LastName",
+        ///        "Program": "Program"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param id="id"></param>
+        /// <returns>An upserted Student</returns>
+        /// <response code="200">Returns the updated Student</response>
+        /// <response code="201">Returns the newly created Student</response>
+        /// <response code="400">If the Car or id is malformed</response>      
+        /// <response code="500">Internal error</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Student>> Update(Guid id, [Bind("FirstName,LastName,Program")] StudentBase studentBase)
+        {
+            Student student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            student.FirstName = studentBase.FirstName;
+            student.LastName = studentBase.LastName;
+            student.Program = studentBase.Program;
+
+            _context.Update(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+    }
+}
